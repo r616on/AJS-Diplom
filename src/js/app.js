519db5b90f8f1ecb140a298ae6dda3cdd98c14ee
Roman Prilepsky <r616on@gmail.com>
2021-05-27 23:05:01 +0300

    init_calcTileType_done

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,17 @@
 /**
  * Entry point of app: don't change this
  */
-import GamePlay from './GamePlay';
-import GameController from './GameController';
-import GameStateService from './GameStateService';
+import GamePlay from "./GamePlay";
+import GameController from "./GameController";
+import GameStateService from "./GameStateService";
+import themes from "./themes";
 
 const gamePlay = new GamePlay();
-gamePlay.bindToDOM(document.querySelector('#game-container'));
+gamePlay.bindToDOM(document.querySelector("#game-container"));
 
 const stateService = new GameStateService(localStorage);
 
 const gameCtrl = new GameController(gamePlay, stateService);
 gameCtrl.init();
-
+gamePlay.drawUi(themes.prairie);
 // don't write your code here
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,16 +1,38 @@
 export function calcTileType(index, boardSize) {
-  // TODO: write logic here
-  return 'center';
+  if (index === 0) {
+    return "top-left";
+  } else if (index + 1 < boardSize) {
+    return "top";
+  } else if (index === boardSize - 1) {
+    return "top-right";
+  } else if (index === boardSize * boardSize - 1) {
+    return "bottom-right";
+  } else if (index === boardSize * boardSize - boardSize) {
+    return "bottom-left";
+  } else if ((index + 1) % boardSize === 0) {
+    return "right";
+  } else if (index % boardSize === 0) {
+    return "left";
+  } else if (index === boardSize * boardSize - 1) {
+    return "bottom-right";
+  } else if (
+    index > boardSize * boardSize - boardSize &&
+    index < boardSize * boardSize - 1
+  ) {
+    return "bottom";
+  } else {
+    return "center";
+  }
 }
 
 export function calcHealthLevel(health) {
   if (health < 15) {
-    return 'critical';
+    return "critical";
   }
 
   if (health < 50) {
-    return 'normal';
+    return "normal";
   }
 
-  return 'high';
+  return "high";
 }

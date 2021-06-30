d05b2e907619855111a146e3768c8cc1642950af
r616on <r616on@gmail.com>
2021-06-30 22:16:07 +0300

    1234

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,15 @@
 /**
  * Entry point of app: don't change this
  */
-import Team from './Team';
-import GamePlay from './GamePlay';
-import GameController from './GameController';
-import GameStateService from './GameStateService';
+import Team from "./Team";
+import GamePlay from "./GamePlay";
+import GameController from "./GameController";
+import GameStateService from "./GameStateService";
 
 const gamePlay = new GamePlay();
-gamePlay.bindToDOM(document.querySelector('#game-container'));
+gamePlay.bindToDOM(document.querySelector("#game-container"));
 const team = new Team();
 const stateService = new GameStateService(localStorage);
 const gameCtrl = new GameController(gamePlay, stateService, team);
 gameCtrl.init();
+console.log("111");

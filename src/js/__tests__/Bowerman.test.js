dce4cf375ca7cda2b8aed2ad5a0041737eeccf09
r616on <69537715+r616on@users.noreply.github.com>
2021-07-01 21:47:07 +0300

    Delete src directory

--- src/js/__tests__/Bowerman.test.js
+++
@@ -1,6 +0,0 @@
-import Bowman from '../person/Bowman';
-
-test('test Bowman ', () => {
-  const data = { attack: 25, defence: 25, type: 'bowman' };
-  expect(new Bowman(1)).toEqual(expect.objectContaining(data));
-});
